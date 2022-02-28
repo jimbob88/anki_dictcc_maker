a6907dac3c06b48f4389b50c1b1558106cd8e028
James Blackburn <blackburnfjames@gmail.com>
2022-02-28 17:13:06 +0000

    Added a fully working anki system

--- anki_maker/__main__.py
+++ anki_maker/__main__.py
@@ -1,5 +1,8 @@
 import genanki
 import os
+import re
+import requests
+
 
 input_data = input("Tampermonkey Output: ").split('¬')
 print(input_data)
@@ -40,3 +43,39 @@ if input_data[-1].startswith('//'):
 if not os.path.exists('audio_files'):
     os.makedirs('audio_files')
 
+get_id_re = r"id=(\d+)"
+_id = re.search(get_id_re, input_data[-1]).group(1)
+
+print("Downloading audio file")
+# wget.download(input_data[-1], f'audio_files/deen_{_id}.mp3')
+headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36'
+}
+r = requests.get(input_data[-1], headers=headers)
+with open(f'audio_files/deen_{_id}.mp3', 'wb') as f:
+    f.write(r.content)
+
+
+
+print("\nCreating anki card")
+note = genanki.Note(
+        model=vocab_model,
+        fields=[
+            input_data[0],
+            input_data[1],
+            input_data[2],
+            input_data[3],
+            f'[sound:deen_{_id}.mp3]'
+            ]
+)
+
+deck = genanki.Deck(
+  2059400110,
+  'Dict.cc Vocab (DE-EN)')
+deck.add_note(note)
+
+pkg = genanki.Package(deck)
+pkg.media_files = [f'audio_files/deen_{_id}.mp3']
+pkg.write_to_file('output.apkg')
+
+print(f"Packaged created at: {os.path.join(os.getcwd(), 'output.apkg')}")
